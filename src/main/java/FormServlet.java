696940075aaf7626279ecc550ee2ce3ccfd3e74a
Phillip Witkin <pwitkin273@gmail.com>
2022-06-02 08:09:35 -0400

    added response in FormServlet to form submission using GET and POST

--- src/main/java/FormServlet.java
+++ src/main/java/FormServlet.java
@@ -30,7 +30,58 @@ public class FormServlet extends HttpServlet{
                 "<html>\n" +
                 "<head><title>" + title + "</title></head>\n" +
                 "<body bgcolor = \"#f0f0f0\">\n" +
-                "<h1 align = \"center\">" + title + "</h1>\n" +
+                "<h1 align = \"center\">" + title + "</h1>\n");// +
+        out.println(
+                "<ul>\n" +
+                        "  <li><b>First Name</b>: "
+                        + firstName + "\n" +
+                        "  <li><b>Last Name</b>: "
+                        + lastName + "\n" +
+                        "  <li><b>Email</b>: "
+                        + email + "\n" +
+                        "</ul>\n"
+        );
+        // link back to the form
+        out.println(
+                "<a href=\"form_get.html\">Fill out form again</a>" +
+                        "</body>" +
+                        "</html>"
+        );
+        out.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // we can get the params from a POST request the same way
+        String firstName = request.getParameter("fname");
+        String lastName = request.getParameter("lname");
+        String email = request.getParameter("email");
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        String title = "Using POST Method to Read Form Data";
+        String docType =
+                "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
+
+        out.println(docType +
+                "<html>\n" +
+                "<head><title>" + title + "</title></head>\n" +
+                "<body bgcolor = \"#f0f0f0\">\n" +
+                "<h1 align = \"center\">" + title + "</h1>\n");// +
+
+        // method adding the same html string as in doGet
+        insertFormData(out, firstName, lastName, email);
+
+        out.println(
+                "<a href=\"form_post.html\">Fill out form again</a>" +
+                "</body>" +
+                "</html>"
+        );
+        out.close();
+    }
+
+    private void insertFormData(PrintWriter out, String firstName, String lastName, String email){
+        out.println(
                 "<ul>\n" +
                 "  <li><b>First Name</b>: "
                 + firstName + "\n" +
@@ -38,20 +89,7 @@ public class FormServlet extends HttpServlet{
                 + lastName + "\n" +
                 "  <li><b>Email</b>: "
                 + email + "\n" +
-                "</ul>\n" +
-                "</body>" +
-                "<a href=\"form_get.html\">Fill out form again</a>" +
-                "</html>"
+                "</ul>\n"
         );
-
-        // just to save time from repeating static html line by line,
-        // we can insert it from a file instead
-//        request.getRequestDispatcher("/form_get.html")
-//                .include(request, response);
-        // Then we can add things
-//        out.println("<h3>And some more HTML added by the method called by a GET request to /form</h3>");
-        out.close();
     }
-
-
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -2,6 +2,7 @@
 <body>
 <h2>Hello World!</h2>
 <h3>This is an html tag inside index.jsp</h3>
-<a href="form_get.html">Fill out my Form!</a>
+<a href="form_get.html">Fill out my Form! (GET)</a>
+<a href="form_post.html">Fill out my Form! (POST)</a>
 </body>
 </html>
