ef43a7b4974d6212531be3615f672d8887a86a10
Phillip Witkin <pwitkin273@gmail.com>
2022-06-02 08:55:41 -0400

    added parameter list after form submission

--- src/main/java/FormServlet.java
+++ src/main/java/FormServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 
 @Log
 @WebServlet("/form")
@@ -30,7 +31,18 @@ public class FormServlet extends HttpServlet{
                 "<html>\n" +
                 "<head><title>" + title + "</title></head>\n" +
                 "<body bgcolor = \"#f0f0f0\">\n" +
-                "<h1 align = \"center\">" + title + "</h1>\n" +
+                "<h1 align = \"center\">" + title + "</h1>\n");
+        // Get all the names of request parameters
+        Enumeration names = request.getParameterNames();
+        out.println("<p>Request Parameter Names are: ");
+        if (names.hasMoreElements()) {
+            out.print(names.nextElement().toString());
+        }
+        do {
+            out.print(", " + names.nextElement().toString());
+        } while (names.hasMoreElements());
+        out.println(".</p>");
+        out.println(
                 "<ul>\n" +
                 "  <li><b>First Name</b>: "
                 + firstName + "\n" +
@@ -40,13 +52,13 @@ public class FormServlet extends HttpServlet{
                 + email + "\n" +
                 "</ul>\n" +
                 "</body>" +
-                "<a href=\"form_get.html\">Fill out form again</a>" +
+                "<a href=\"form-get.html\">Fill out form again</a>" +
                 "</html>"
         );
 
         // just to save time from repeating static html line by line,
         // we can insert it from a file instead
-//        request.getRequestDispatcher("/form_get.html")
+//        request.getRequestDispatcher("/form-get.html")
 //                .include(request, response);
         // Then we can add things
 //        out.println("<h3>And some more HTML added by the method called by a GET request to /form</h3>");
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -2,6 +2,6 @@
 <body>
 <h2>Hello World!</h2>
 <h3>This is an html tag inside index.jsp</h3>
-<a href="form_get.html">Fill out my Form!</a>
+<a href="form-get.html">Fill out my Form!</a>
 </body>
 </html>
